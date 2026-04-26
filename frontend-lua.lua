30e31169a757b33a0c1c94a048c640767e214f16
Konkitoman <konkitoman@users.noreply.github.com>
2026-04-26 15:40:29 +0300

    chore: resolve TODO, it was not a precedence problem, it was an actual bug, also improved an error.

--- frontend-lua.lua
+++ frontend-lua.lua
@@ -632,10 +632,8 @@ ast_parse_path_ = function(C, S)
 					error("Cannot read path at: " .. S .. "-" .. s.span[2])
 				end
 				s = C[1][C[2]]
-				-- frontend is not parsing this correctly!
-				-- TODO: more work needs to be done for precedence.
-				if not s and (s.T ~= "p" or s.i ~= "]") then
-					error("Cannot path with expresion is not ending in `]` at: " .. S .. "-" .. s.span[2])
+				if not s or s.T ~= "p" or s.i ~= "]" then
+					error("path, path segment that starts with `[` is expected to end with `]`, at: " .. S .. "-" .. s.span[2])
 				end
 				C[2] = C[2] + 1
 				local r = ast_parse_path_(C, S)
